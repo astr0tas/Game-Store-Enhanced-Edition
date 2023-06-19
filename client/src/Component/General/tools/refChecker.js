6430df84e7117d3f8c7549b67ff466828cb826e0
NghiaDuong <101704565+astr0tas@users.noreply.github.com>
2023-06-20 00:15:07 +0700

    Update refChecker.js

--- client/src/Component/General/tools/refChecker.js
+++ client/src/Component/General/tools/refChecker.js
@@ -1,13 +1,13 @@
 export const isRefValid = (ref, idx = -1) =>
 {
       if (idx === -1)
-            return ref.current !== null && ref.current !== undefined;
-      return ref.current[idx] !== null && ref.current[idx] !== undefined;
+            return ref.current !== null && ref.current !== undefined && ref.current !== '' && ref.current !== 'undefined' && ref.current !== 'null';
+      return ref.current[idx] !== null && ref.current[idx] !== undefined && ref.current[idx] !== '' && ref.current[idx] !== 'undefined' && ref.current[idx] !== 'null';
 }
 
 export const isRefNotValid = (ref, idx = -1) =>
 {
       if (idx === -1)
-            return ref.current === null || ref.current === undefined;
-      return ref.current[idx] === null || ref.current[idx] === undefined;
+            return ref.current === null || ref.current === undefined || ref.current === '' || ref.current === 'undefined' || ref.current === 'null';
+      return ref.current[idx] === null || ref.current[idx] === undefined || ref.current[idx] === '' || ref.current[idx] === 'undefined' || ref.current[idx] === 'null';
 }
